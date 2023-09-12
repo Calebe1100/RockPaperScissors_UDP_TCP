905b62aae8abea0e6a054ef6242aeff3755340ac
Calebe Libério <calebe.liberio@vertown.com>
2023-09-11 21:41:40 -0300

    fix

--- client/src/ConfigSocketController.java
+++ client/src/ConfigSocketController.java
@@ -15,7 +15,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
@@ -35,8 +34,6 @@ public class ConfigSocketController {
                 "Tesoura");
 
         selectOption.setItems(items);
-
-        fieldResult.setVisible(false);
     }
 
     @FXML
@@ -56,9 +53,6 @@ public class ConfigSocketController {
 
     @FXML
     private ChoiceBox<String> selectOption;
-
-    @FXML
-    private TextArea fieldResult;
 
     @FXML
     private AnchorPane connectionGroup;
